a972a12aba9a36caf9a6f4c44f4d17c954662ce8
LitFill <litfill@litfill.site>
2024-07-05 03:03:19 +0700

    feat(core)!: Revamp how to use this library
    now provide two brand new functions to create log file and logger.
    
    use wrapping as describe in slog package documentation with little modification so the source of the log is correct.
    
    now Log and Assign want logger as parameter.

--- fatal.go
+++ fatal.go
@@ -3,47 +3,100 @@
 package fatal
 
 import (
+	"context"
 	"io"
 	"log/slog"
 	"os"
+	"runtime"
+	"time"
 )
 
-type writer struct {
-	files []io.Writer
+func CreateLogFile(filename string) *os.File {
+	log_file := Assign(os.OpenFile(
+		filename,
+		os.O_CREATE|os.O_WRONLY|os.O_APPEND,
+		0666,
+	))(
+		slog.Default(),
+		"cannot create file",
+		"file name", filename,
+	)
+	return log_file
 }
 
-func (w writer) Write(p []byte) (n int, err error) {
-	for _, f := range w.files {
-		n, err = f.Write(p)
+func CreateLogger(w io.Writer, lev slog.Level) *slog.Logger {
+	logger := slog.New(slog.NewJSONHandler(
+		io.MultiWriter(w, os.Stderr),
+		&slog.HandlerOptions{
+			AddSource: true,
+			Level:     lev,
+		},
+	))
+	return logger
+}
+
+// Debug is a function that wraps slog.
+// The log record contains the source position of the caller of Debug.
+func Debug(logger *slog.Logger, msg string, args ...any) {
+	if !logger.Enabled(context.Background(), slog.LevelDebug) {
+		return
 	}
-	return
+	var pcs [1]uintptr
+	runtime.Callers(2, pcs[:]) // skip [Callers, Debugf]
+	r := slog.NewRecord(time.Now(), slog.LevelDebug, msg, pcs[0])
+	r.Add(args...)
+	err := logger.Handler().Handle(context.Background(), r)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func NewWriter(writers []io.Writer) io.Writer {
-	return writer{writers}
+// Info is a function that wraps slog.
+// The log record contains the source position of the caller of Info.
+func Info(logger *slog.Logger, msg string, args ...any) {
+	if !logger.Enabled(context.Background(), slog.LevelInfo) {
+		return
+	}
+	var pcs [1]uintptr
+	runtime.Callers(2, pcs[:]) // skip [Callers, Infof]
+	r := slog.NewRecord(time.Now(), slog.LevelInfo, msg, pcs[0])
+	r.Add(args...)
+	err := logger.Handler().Handle(context.Background(), r)
+	if err != nil {
+		panic(err)
+	}
 }
 
-var myWriter = NewWriter([]io.Writer{os.Stderr})
-
-var logger = slog.New(slog.NewJSONHandler(myWriter, &slog.HandlerOptions{AddSource: true}))
-
-func Error(msg string, log ...any) { logger.Error(msg, log...) }
-func Debug(msg string, log ...any) { logger.Debug(msg, log...) }
-func Info(msg string, log ...any)  { logger.Info(msg, log...) }
+// Error is a function that wraps slog.
+// The log record contains the source position of the caller of Error.
+func Error(logger *slog.Logger, msg string, args ...any) {
+	if !logger.Enabled(context.Background(), slog.LevelError) {
+		return
+	}
+	var pcs [1]uintptr
+	runtime.Callers(2, pcs[:]) // skip [Callers, Infof]
+	r := slog.NewRecord(time.Now(), slog.LevelError, msg, pcs[0])
+	r.Add(args...)
+	err := logger.Handler().Handle(context.Background(), r)
+	if err != nil {
+		panic(err)
+	}
+}
 
 // Log wraps function call returning error to log it using `log/slog` so it has `msg` and `log`.
 // example:
 //
 //	Log(http.ServeAndListen(port),
+//		myLogger,
 //	    "Can not serve and listen",
 //	    "port", port
 //	)
-func Log(err error, msg string, log ...any) {
+func Log(err error, logger *slog.Logger, msg string, args ...any) {
 	if err == nil {
 		return
 	}
-	Error(err.Error())
-	Error(msg, log...)
+	local_logger := logger.With("err", err)
+	Error(local_logger, msg, args...)
 	os.Exit(1)
 }
 
@@ -51,18 +104,19 @@ func Log(err error, msg string, log ...any) {
 // returns a function that takes msg and logs in style of slog.logger and return the val T.
 //
 //	file := Assign(os.Create(fileName))(
+//		myLogger,
 //	    "cannot create file",
 //	    "filename", fileName,
 //	)
-func Assign[T any](val T, err error) func(string, ...any) T {
+func Assign[T any](val T, err error) func(*slog.Logger, string, ...any) T {
 	if err == nil {
-		return func(msg string, log ...any) T {
+		return func(logger *slog.Logger, msg string, log ...any) T {
 			return val
 		}
 	}
-	return func(msg string, log ...any) T {
-		Error(err.Error())
-		Error(msg, log...)
+	return func(logger *slog.Logger, msg string, args ...any) T {
+		local_logger := logger.With("err", err)
+		Error(local_logger, msg, args...)
 		return val
 	}
 }
